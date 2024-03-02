4ef5dd37a07d938caf7b6657c8c0832e7ddba083
diogojgferreira <133152545+diogojgferreira@users.noreply.github.com>
2024-03-02 22:17:01 +0000

    Add files via upload

--- ImportarDados.m
+++ ImportarDados.m
@@ -1,13 +1,17 @@
-path = "C:\Users\joaog\OneDrive\Documentos\MATLAB\44\";
+function dados = ImportarDados(numFich)
+path = "archive\data\44\";
 constantPath = "_44_0.wav";
 dados = cell(1,numFich);
+
+limiar = 0.1
+tamanhoMaximo = 0;
 
 for num = 0:numFich-1
     nomeAudio = strcat(path,num2str(num),constantPath);
 
     [amplitude,amostras] = audioread(nomeAudio);
 
-    sinalNormalizado = amplitude / max(abs(amplitude));
-    dados{num+1}.dadosSinal = sinalNormalizado;
+    amplitude = amplitude ./ max(abs(amplitude));
+    dados{num+1}.amplitude = amplitude;
     dados{num+1}.dadosFreq = amostras;
 end
